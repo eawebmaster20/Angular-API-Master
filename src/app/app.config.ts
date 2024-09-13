553701d84357e4bcd6556628e4af2ffc11689ac7
eawebmaster20 <eaweb20@gmail.com>
2024-09-13 18:12:38 +0000

    inject interceptors into the http provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { headerModifierInterceptor } from './services/Http-interceptors/request.interceptors';
+import { loggerInterceptor } from './services/Http-interceptors/logger.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), 
+    provideHttpClient(withInterceptors([headerModifierInterceptor, loggerInterceptor]))
+  ]
 };
