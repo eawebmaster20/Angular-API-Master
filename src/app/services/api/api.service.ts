2dc1c38d1b70c48dea0cf354e9afb47035d456dc
eawebmaster20 <eaweb20@gmail.com>
2024-09-13 18:13:02 +0000

    feat: create base endpoints

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private  http:HttpClient) { }
+
+  getPost(): Observable<any>{
+    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  }
+
+  createPost(post:any): Observable<any>{
+    return this.http.post('https://jsonplaceholder.typicode.com/comments',{});
+  }
+
+  getPostComments(postId:string): Observable<any>{
+    return this.http.get('https://jsonplaceholder.typicode.com/posts/1/comments');
+  }
+
+  updatePost(postId:string): Observable<any>{
+    return this.http.put('https://jsonplaceholder.typicode.com/users',{});
+  }
+
 }
