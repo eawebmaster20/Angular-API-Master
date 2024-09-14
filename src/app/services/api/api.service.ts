86a8350273ba0d6a1566ea23f31ea495c0bffc66
eawebmaster20 <eaweb20@gmail.com>
2024-09-14 22:15:32 +0000

    core: update api calls

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,19 @@ export class ApiService {
   constructor(private  http:HttpClient) { }
 
   getPost(): Observable<any>{
-    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+    return this.http.get(`${environment.BASE_API}/posts`);
   }
 
   createPost(post:any): Observable<any>{
-    return this.http.post('https://jsonplaceholder.typicode.com/comments',{});
+    return this.http.post(`${environment.BASE_API}/posts`,post);
   }
 
   getPostComments(postId:string): Observable<any>{
-    return this.http.get('https://jsonplaceholder.typicode.com/posts/1/comments');
+    return this.http.get(`${environment.BASE_API}/comments?postId=1`);
   }
 
-  updatePost(postId:string): Observable<any>{
-    return this.http.put('https://jsonplaceholder.typicode.com/users',{});
+  updatePost(postId:string, data:any): Observable<any>{
+    return this.http.patch(`${environment.BASE_API}/posts/${postId}`,data);
   }
 
 }
