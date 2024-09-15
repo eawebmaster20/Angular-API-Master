46d7e1c9d4f1a856a9472c7202c954cb9ff8f583
eawebmaster20 <eaweb20@gmail.com>
2024-09-15 13:02:27 +0000

    feat: setup and configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'post-form', pathMatch: 'full' },
+    { path: 'home', loadComponent: () =>import('./components/list/list.component').then((res) =>res.ListComponent)},
+    { path: 'post-form', loadComponent: () =>import('./components/post-form/post-form.component').then((res) =>res.PostFormComponent)},
+    { path: 'update/:id', loadComponent: () =>import('./components/post-form/post-form.component').then((res) =>res.PostFormComponent)},
+    { path: '**', redirectTo: 'post-form' }
+];
