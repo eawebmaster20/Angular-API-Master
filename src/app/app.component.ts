aac25b7c4e57b1952d934540f6495a901a6f25e0
eawebmaster20 <eaweb20@gmail.com>
2024-09-15 13:00:51 +0000

    feat: clean template and inject api service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './shared/services/api/api.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angularApiMaster';
-  constructor(private http:HttpClient){
-    this.http.get('http://localhost:3000').subscribe(res=>console.log(res))
+  constructor(private apiService: ApiService){
+    // this.apiService.deletePost(1).subscribe(res=>{
+    //   console.log(res)
+    //   this.apiService.getPosts().subscribe(res=>console.log(res))
+    // })
   }
 }
