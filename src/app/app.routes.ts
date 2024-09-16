ed484a6fd9ccae19fea7460d0242a007349bd6db
eawebmaster20 <eaweb20@gmail.com>
2024-09-16 05:40:01 +0000

    core: add route to  a specific post

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', redirectTo: 'post-form', pathMatch: 'full' },
     { path: 'home', loadComponent: () =>import('./components/list/list.component').then((res) =>res.ListComponent)},
+    { path: 'post/:id', loadComponent: () =>import('./components/post-details/post-details.component').then((res) =>res.PostDetailsComponent)},
     { path: 'post-form', loadComponent: () =>import('./components/post-form/post-form.component').then((res) =>res.PostFormComponent)},
     { path: 'update/:id', loadComponent: () =>import('./components/post-form/post-form.component').then((res) =>res.PostFormComponent)},
     { path: '**', redirectTo: 'post-form' }
