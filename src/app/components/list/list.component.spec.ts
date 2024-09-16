67095b3de0c7ec7b5cedfe6909ea6e5e698365b6
eawebmaster20 <eaweb20@gmail.com>
2024-09-16 05:40:39 +0000

    feat: add skeleton loading

--- src/app/components/list/list.component.spec.ts
+++ src/app/components/list/list.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import { DataService } from '../../shared/services/data/data.service';
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let dataService: jasmine.SpyObj<DataService>;
 
   beforeEach(async () => {
+    const dataServiceSpy = jasmine.createSpyObj('DataService', ['loadPost'], { count: 0 });
     await TestBed.configureTestingModule({
-      imports: [ListComponent]
+      imports: [ListComponent],
+      providers: [{ provide: DataService, useValue: dataServiceSpy }],
     })
     .compileComponents();
     
@@ -19,5 +23,22 @@ describe('ListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should increment count on scroll down', () => {
+    dataService.count = 0;
+    component.onScrollDown();
+    expect(dataService.count).toBe(1);
+  });
+
+  it('should call loadPost if count is less than 10', () => {
+    dataService.count = 9;
+    component.onScrollDown();
+    expect(dataService.loadPost).toHaveBeenCalled();
+  });
+
+  it('should not call loadPost if count is 10 or more', () => {
+    dataService.count = 10;
+    component.onScrollDown();
+    expect(dataService.loadPost).not.toHaveBeenCalled();
   });
 });
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -4,11 +4,13 @@ import { ApiService } from '../../shared/services/api/api.service';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { IPost } from '../../shared/interfaces/post';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [AsyncPipe, RouterLink],
+  imports: [AsyncPipe, RouterLink,InfiniteScrollModule, SkeletonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
@@ -16,13 +18,8 @@ export class ListComponent {
   constructor(public dataService:DataService, private apiService: ApiService){
     dataService.loadPost()
   }
-  @HostListener('scroll', ['$event'])
-  onScroll(event: any): void {
-    // const pos = (document.documentElement.scrollTop || document.body.scrollTop) + window.innerHeight;
-    // const max = document.documentElement.scrollHeight;
-    // if (pos === max) {
-    //   this.dataService.loadItems();
-    // }
-    console.log(event);
+  onScrollDown(){
+    this.dataService.count++;
+    this.dataService.count <10 ? this.dataService.loadPost(): '';
   }
 }
